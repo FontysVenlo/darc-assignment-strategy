fb1c7d303f915b493141fb06d6141544d8907a34
Stefan Sobek <s.sobek@fontys.nl>
2020-10-07 14:00:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# DARC Assignment - Strategy Pattern
+
---
+++ src/nl/fontys/darc/Main.java
@@ -0,0 +1,8 @@
+package nl.fontys.darc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("DARC-assignment-1");
+    }
+}
